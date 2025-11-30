7d675693c04590a7d7fd795b3b63080419f30536
Ryuho13 <hosedk99@gmail.com>
2025-11-30 22:54:45 +0900

    오류 처리

--- src/main/java/com/spring/app/board/notice/NoticeController.java
+++ src/main/java/com/spring/app/board/notice/NoticeController.java
@@ -8,8 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.spring.app.board.BoardDTO;
 import com.spring.app.util.Pager;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,15 +26,14 @@ public class NoticeController {
 	
 	@GetMapping("list")
 	public void list(Pager pager, Model model)throws Exception{
-		//1. totalCount 구하기
-		List<NoticeDTO> list = noticeService.list(pager);
+		List<BoardDTO> list = noticeService.list(pager);
 		model.addAttribute("list", list);
 		model.addAttribute("pager", pager);
 	}
 	@GetMapping("detail")
 	public String detail(NoticeDTO noticeDTO, Model model)throws Exception{
-		noticeDTO = noticeService.detail(noticeDTO);
-		model.addAttribute("dto", noticeDTO);
+		BoardDTO boardDTO = noticeService.detail(noticeDTO);
+		model.addAttribute("dto", boardDTO);
 		
 		return "notice/detail";
 	}
--- src/main/java/com/spring/app/board/notice/NoticeService.java
+++ src/main/java/com/spring/app/board/notice/NoticeService.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
+import com.spring.app.board.BoardDTO;
 import com.spring.app.util.Pager;
 
 @Service
@@ -14,9 +14,9 @@ public class NoticeService {
 	@Autowired
 	private NoticeDAO noticeDAO;
 	
-	public List<NoticeDTO> list(Pager pager) throws Exception {
+	public List<BoardDTO> list(Pager pager) throws Exception {
 	    // 1. 전체 글의 갯수 DB에서 조회
-	    Long totalCount = noticeDAO.count();
+	    Long totalCount = noticeDAO.count(pager);
 
 	    // 2. 조회된 totalCount를 Pager에 전달해서 페이지 계산하기
 	    pager.pageing(totalCount);
@@ -24,7 +24,7 @@ public class NoticeService {
 	    // 3. 계산된 Pager 정보를 DAO로 전달해서 목록 가져오기
 	    return noticeDAO.list(pager);
 	}
-	public NoticeDTO detail(NoticeDTO noticeDTO)throws Exception{
+	public BoardDTO detail(NoticeDTO noticeDTO)throws Exception{
 		return noticeDAO.detail(noticeDTO);
 	}
 	public int add(NoticeDTO noticeDTO)throws Exception{
