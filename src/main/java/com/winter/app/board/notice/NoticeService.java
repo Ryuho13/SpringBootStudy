196af504235feabd169510f5d3f1a8bf459433e3
Ryuho13 <hosedk99@gmail.com>
2025-12-08 00:48:40 +0900

    asdaa

--- src/main/java/com/winter/app/board/notice/NoticeService.java
+++ src/main/java/com/winter/app/board/notice/NoticeService.java
@@ -3,11 +3,13 @@ package com.winter.app.board.notice;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.winter.app.board.BoardDTO;
@@ -19,6 +21,7 @@ import com.winter.app.util.Pager;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class NoticeService implements BoardService {
+
 	@Autowired
 	private NoticeDAO noticeDAO;
 	
@@ -28,47 +31,44 @@ public class NoticeService implements BoardService {
 	@Value("${app.upload.notice}")
 	private String uploadPath;
 	
+	
 	@Override
-	public List<BoardDTO> list(Pager pager) throws Exception {
+	public List<BoardDTO> list (Pager pager)throws Exception{
 		//1. totalCount 구하기
-		Long totalCount = noticeDAO.count(pager);
+		Long totalCount= noticeDAO.count(pager);
+		
 		pager.pageing(totalCount);
+
+		
 		return noticeDAO.list(pager);
 	}
 	
 	@Override
-	public BoardDTO detail(BoardDTO boardDTO) throws Exception {
+	public BoardDTO detail(BoardDTO boardDTO)throws Exception{
+		
 		return noticeDAO.detail(boardDTO);
 	}
-	
-	
-	
 	@Override
-	public int update(BoardDTO boardDTO) throws Exception {
-		return noticeDAO.update(boardDTO);
-	}
-	
-	
-	@Override
-	public int add(BoardDTO boardDTO, MultipartFile[] attach) throws Exception {
-		// 1. 파일을 HDD에 저장
-		//	1) 어디에 저장?
-		//	2) 어떤 이름으로 저장?
-		
+	public int add(BoardDTO boardDTO, MultipartFile [] attach)throws Exception{
+		//글번호가 필요
 		int result = noticeDAO.add(boardDTO);
-		if (attach == null) {
+		
+
+		
+		if(attach == null) {
 			return result;
 		}
 		
+		//1. 파일을 HDD에 저장
+		//   1) 어디에 저장?
+		//   2) 어떤 이름으로 저장?
 		File file = new File(uploadPath);
 		
-		for (MultipartFile f : attach) {
-			if (f == null || f.isEmpty() || f.getSize() == 0) {
+		for(MultipartFile f: attach) {
+			if(f==null || f.isEmpty()) {
 				continue;
 			}
-			
 			String fileName = fileManager.fileSave(file, f);
-			
 			//4. 정보를 DB에 저장
 			BoardFileDTO boardFileDTO = new BoardFileDTO();
 			boardFileDTO.setFileName(fileName);
@@ -76,30 +76,36 @@ public class NoticeService implements BoardService {
 			boardFileDTO.setBoardNum(boardDTO.getBoardNum());
 			noticeDAO.fileAdd(boardFileDTO);
 		}
-		return  result;
+		
+		
+		return result;//noticeDAO.add(boardDTO);
 	}
-	
-	
 	@Override
-	public int delete(BoardDTO boardDTO) throws Exception {
+	public int update(BoardDTO boardDTO)throws Exception{
+		return noticeDAO.update(boardDTO);
+	}
+	@Override
+	public int delete(BoardDTO boardDTO)throws Exception{
 		boardDTO = noticeDAO.detail(boardDTO);
-		// HDD에서 파일을 삭제 하는 코드
-		if (boardDTO.getBoardFileList() != null) {
-			for (BoardFileDTO boardFileDTO : boardDTO.getBoardFileList()) {
+		//HDD에서 파일을 삭제
+		if(boardDTO.getFileDTOs() != null) {
+			for(BoardFileDTO boardFileDTO:boardDTO.getFileDTOs()) {
 				File file = new File(uploadPath, boardFileDTO.getFileName());
 				boolean flag = fileManager.fileDelete(file);
+				
 			}
 		}
-		//----------------------
-		noticeDAO.delete(boardDTO);
+		
+		//---------------
+		int result = noticeDAO.fileDelete(boardDTO);
 		return noticeDAO.delete(boardDTO);
 	}
 	
 	@Override
 	public BoardFileDTO fileDetail(BoardFileDTO boardFileDTO) throws Exception {
+		// TODO Auto-generated method stub
 		
 		return noticeDAO.fileDetail(boardFileDTO);
 	}
-	
 	
 }
